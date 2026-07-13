405c232a11c5f4b2a0b3bb6d3887303bdad17f66
zeyad <aziad711@gmail.com>
2026-07-13 19:54:06 +0300

    ✅ NEW FUNCTION ADDED : GENERATE OTP

--- other/password.go
+++ other/password.go
@@ -7,3 +7,10 @@ func HiddenPassword(password string)string{
 	}
 	return hidden
 }
+// GENERATE RANDOM OTP 
+var seed uint64 = 12345
+
+func GenerateOTP() uint64 {
+	seed = (1664525*seed + 1013904223) % 4294967296
+	return 1000 + (seed % 9000)
+}
