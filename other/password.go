df57e26c1e37a7b48cbfd6efd38c00888d563fcd
zeyad <aziad711@gmail.com>
2026-07-13 19:58:37 +0300

    ✅ NEW FUNCTION ADDED : GENERATE RANDOM PASSWORD AS NUMBERS

--- other/password.go
+++ other/password.go
@@ -1,4 +1,5 @@
 package other
+var seed uint64 = 12345
 // HASH PASSWORD 
 func HiddenPassword(password string)string{
 	var hidden string;
@@ -8,9 +9,12 @@ func HiddenPassword(password string)string{
 	return hidden
 }
 // GENERATE RANDOM OTP 
-var seed uint64 = 12345
-
 func GenerateOTP() uint64 {
 	seed = (1664525*seed + 1013904223) % 4294967296
 	return 1000 + (seed % 9000)
+}
+// GENERATE RANDOM PASSWORD 
+func GeneratePasswordAsNumbers(length uint64) uint64 {
+	seed = (1664525*seed + 1013904223) % 4294967296
+	return (length*1000) + (seed % (length*9000))
 }
